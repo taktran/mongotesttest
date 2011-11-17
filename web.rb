65e0b62e3a78f0c049622b0dc1db847d7d568414
Tak Tran <contact@tutaktran.com>
2011-11-17 17:18:06 +0000

    Cleaned up web file.

--- web.rb
+++ web.rb
@@ -1,14 +1,10 @@
 require 'sinatra'
 require 'sinatra/mongomapper'
 
+# Mongo mapper settings
 set :mongomapper, 'mongomapper://localhost:27017/mongotesttest-example'
-
-# Specify a logger to be used by the MongoDB driver
-# Value can be any that the Logger accepts for initialization
-# The following is the default setting
-environment = "development"
-set :mongo_logfile, File.join("log", "mongo-driver-environment.log")
+set :mongo_logfile, File.join("log", "mongo-driver-#{settings.environment}.log")
 
 get '/' do
-  "hello ya ya ya"
+  "Mongo log file: #{settings.mongo_logfile}"
 end
