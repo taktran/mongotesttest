e6cc1482a9961ee1ccfed7d42ba582ef5b75b0a7
Tak Tran <contact@tutaktran.com>
2011-11-17 18:02:31 +0000

    Add create and delete methods.

--- web.rb
+++ web.rb
@@ -13,12 +13,33 @@ class TeamMember
 end
 
 get '/' do
-  TeamMember.create(:first_name => "Billy", :last_name => "Corgan")
-  first_billy = TeamMember.find_by_first_name('Billy')
-  output = "<p>Billy who? #{first_billy.last_name}</p>"
+  output = ""
   
-  billys = TeamMember.find_all_by_first_name('Billy')
-  output += "<p>Count: #{billys.length}</p>"
+  first_billy = TeamMember.find_by_first_name('Billy')
+  if first_billy.present?
+    output = "<p>Billy who? #{first_billy.last_name}</p>"
+  
+    billys = TeamMember.find_all_by_first_name('Billy')
+    output += "<p>Count: #{billys.length}</p>"
+  end
+  
+  output += "<p><a href='/create'>Create another Corgan</a></p>"
+  output += "<p><a href='/delete'>Delete</a></p>"
   
   output
+end
+
+# Bad practice to do this in get request but hey, this is a hack
+get '/create' do
+  TeamMember.create(:first_name => "Billy", :last_name => "Corgan")
+  
+  redirect '/'
+end
+
+# Bad practice to do this in get request but hey, this is a hack
+get '/delete' do
+  first_billy = TeamMember.find_by_first_name('Billy')
+  first_billy.destroy unless first_billy.nil?
+  
+  redirect '/'
 end
