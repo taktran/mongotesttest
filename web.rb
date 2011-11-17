27b79f7d6b2af2da72173a88962616672f0d5640
Tak Tran <contact@tutaktran.com>
2011-11-17 17:29:46 +0000

    Simple example of how to add a new record in mongo.

--- web.rb
+++ web.rb
@@ -5,6 +5,20 @@ require 'sinatra/mongomapper'
 set :mongomapper, 'mongomapper://localhost:27017/mongotesttest-example'
 set :mongo_logfile, File.join("log", "mongo-driver-#{settings.environment}.log")
 
+class TeamMember
+  include MongoMapper::Document
+
+  key :first_name, String
+  key :last_name, String
+end
+
 get '/' do
-  "Mongo log file: #{settings.mongo_logfile}"
+  TeamMember.create(:first_name => "Billy", :last_name => "Corgan")
+  first_billy = TeamMember.find_by_first_name('Billy')
+  output = "<p>Billy who? #{first_billy.last_name}</p>"
+  
+  billys = TeamMember.find_all_by_first_name('Billy')
+  output += "<p>Count: #{billys.length}</p>"
+  
+  output
 end
